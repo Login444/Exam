349945f7aeb5d604be43e5f3ee4210dfa31e152b
Artemiy <birukovartemij@gmail.com>
2023-02-15 21:42:24 +0300

    задача решена, осталось заполнить файл readme и подкорректировать блок-схему

---
+++ Prog/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] GetArray(int size)
+
+{
+    string[] result = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Ваше {i+1}-я строка: ");
+        result[i] = Convert.ToString(Console.ReadLine());
+    }
+
+    return result;
+}
+
+void ShowNewArray(string[] array)
+{
+    Console.WriteLine("Вот строки длинна которых меньше либо равна 3: ");
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Сколько строк вы хотите проверить?");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string[] arr = GetArray(a);
+
+
+ShowNewArray(arr);
